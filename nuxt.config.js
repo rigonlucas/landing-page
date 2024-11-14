83fcc7999ac5f5385b1c4522e62257b48ff602e5
rigonlucas <lucas.gelhen.rigon@gmail.com>
2024-11-13 21:24:17 -0300

    teste PR

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - landing-page',
-    title: 'landing-page',
+    title: 'landing-page teste',
     htmlAttrs: {
       lang: 'en'
     },
